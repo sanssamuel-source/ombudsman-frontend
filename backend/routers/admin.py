62dcea719494255bf8b15187b631cfef21f4c8af
sanssamuel-source <sansconstruction16@gmail.com>
2025-11-27 20:41:55 +0000

    Add files via upload
    Fix CORS settings

--- backend/routers/admin.py
+++ backend/routers/admin.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Header
 from sqlalchemy.orm import Session
-from .. import models, schemas, database
+import models, schemas, database
 from typing import List
 
 router = APIRouter()
