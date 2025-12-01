5c813979fb928ba2e2b47668f437396edf992ea7
sanssamuel-source <sansconstruction16@gmail.com>
2025-12-01 10:33:47 +0000

    Add files via upload

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import tempfile
+import os
 
 # Use system temp directory for database (works on Windows & Linux)
 tmp_dir = tempfile.gettempdir()
