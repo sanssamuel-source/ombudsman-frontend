f9e8667000ddc2ff3264979d94f1b776ddc5ed42
sanssamuel-source <sansconstruction16@gmail.com>
2025-12-01 16:53:38 +0000

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import models, database
-from routers import public, admin, sms, ussd
+from routers import public, admin, sms
 
 # Create tables
 models.Base.metadata.create_all(bind=database.engine)
@@ -20,7 +20,6 @@ app.add_middleware(
 app.include_router(public.router, prefix="/api/public", tags=["Public"])
 app.include_router(admin.router, prefix="/api/admin", tags=["Admin"])
 app.include_router(sms.router, tags=["SMS"])
-app.include_router(ussd.router, prefix="/api/ussd", tags=["USSD"])
 
 @app.get("/")
 def read_root():
