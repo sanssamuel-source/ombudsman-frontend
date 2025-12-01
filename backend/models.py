6f51f27bc84dd96cc53ec68c5d32a8345c3484a2
sanssamuel-source <sansconstruction16@gmail.com>
2025-12-01 08:58:57 +0000

    Add files via upload

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from .database import Base
+from database import Base
 import uuid
 
 def generate_reference_id():
